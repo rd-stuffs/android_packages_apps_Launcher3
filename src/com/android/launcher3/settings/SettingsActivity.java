79bec6f77190ee0ed8fe3889d8d85cf203da6f0a
Abhay Singh Gill <abhaygill017@gmail.com>
2024-08-03 01:42:27 +0800

    Launcher3: Add Circle To Search
    Requires framework overlays:
    
    <string name="config_defaultContextualSearchPackageName" translatable="false">com.google.android.googlequicksearchbox</string>
    
    Change-Id: Ia0b03781ad579d88e62ef9cc5721fc5cdd0c943b
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -36,6 +36,7 @@ import static co.aospa.launcher.OverlayCallbackImpl.KEY_ICON_SIZE;
 import static co.aospa.launcher.OverlayCallbackImpl.KEY_MINUS_ONE;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -72,6 +73,9 @@ import com.android.launcher3.states.RotationHelper;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.SettingsCache;
+
+import com.android.quickstep.util.AssistUtils;
+
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
 /**
@@ -94,6 +98,12 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
 
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
+
+    private static final String CTS_KEY = "pref_allow_cts";
+    private static boolean mContextualSearchDefValue;
+    private static boolean mCtsEnabled;
+
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -126,6 +136,9 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
             // Display the fragment as the main content.
             fm.beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, f).commit();
         }
+        mContext = getApplicationContext();
+        mContextualSearchDefValue = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_searchAllEntrypointsEnabledDefault);
     }
 
     private boolean startPreference(String fragment, Bundle args, String key) {
@@ -184,6 +197,7 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
         private Preference mShowGoogleAppPref;
         private Preference mShowGoogleBarPref;
         private Preference mThemeAllAppsIconsPref;
+        private Preference mCtsPref;
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -289,6 +303,11 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                     InvariantDeviceProfile.INSTANCE.get(getContext())
                             .onConfigChanged(getActivity().getApplicationContext());
                     break;
+                case CTS_KEY:
+                    mCtsEnabled = LauncherPrefs.getPrefs(mContext).getBoolean(CTS_KEY, mContextualSearchDefValue);
+                    Settings.Secure.putInt(mContext.getContentResolver(),
+                            Settings.Secure.SEARCH_ALL_ENTRYPOINTS_ENABLED, mCtsEnabled ? 1 : 0);
+                    break;
                 default:
                     break;
             }
@@ -327,6 +346,12 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                 case KEY_ALLAPPS_THEMED_ICONS:
                     mThemeAllAppsIconsPref = preference;
                     updateThemeAllAppsIconsPref();
+                    return true;
+
+                case CTS_KEY:
+                    mCtsPref = preference;
+                    preference.setEnabled(AssistUtils.newInstance(
+                            getContext()).isContextualSearchIntentAvailable());
                     return true;
 
                 case DEVELOPER_OPTIONS_KEY:
